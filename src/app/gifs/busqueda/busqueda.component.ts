08fba60157ad34b0e639d12e7c3fd18f2c7babb6
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2021-04-15 02:23:04 -0300

    Seccion 7. 80 GifsService

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -10,11 +11,14 @@ export class BusquedaComponent {
 
   @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
+  constructor(private gifsService: GifsService){}
+
   buscar(){
-    const valor = this.txtBuscar.nativeElement.value;
-    console.log(valor);     
-
+    const valor = this.txtBuscar.nativeElement.value; 
+    
+    this.gifsService.buscarGifs(valor);
+    
     this.txtBuscar.nativeElement.value = '';//borra la caja de texto despues de presionar enter
-  }
 
+  }
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,11 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SidebarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class SidebarComponent {
+  
+  /*
+    get historial() es de la propiedad de esta clase SidebarComponent
+    lo cual sirve para modificar el html 
+  */
+  get historial(){
+    return this.gifsService.historial; //este es del servicio
   }
+
+  constructor(private gifsService: GifsService){}
 
 }
