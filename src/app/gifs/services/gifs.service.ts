15262b36b07f700d7a00d4c5fe3ca7b0bf114b53
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2021-04-17 08:54:56 -0300

    Seccion 7. 83 Peticion HTTP fetch() async/await

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,22 +13,12 @@ export class GifsService {
       return [...this._historial];
     }
 
-    buscarGifs( query: string = '' ){
+    async buscarGifs( query: string = '' ){
       
-      query = query.trim().toLowerCase();//borra espacios adelante/atras y convierte a minuscula
-
-     /* solo va insertar si no existe */
-      if( !this._historial.includes(query) ){
-        this._historial.unshift(query);//inserta al inicio
-        this._historial = this._historial.splice(0,10);//asigna a historial elemento 0 a 10
-      }
-
-      fetch('https://api.giphy.com/v1/gifs/search?api_key=E1Xs92016grdQgcmMAnfn0bSA6iBk9HV&q=simpsons&limit=10')
-       .then(resp => {
-         resp.json().then(data => {
-          console.log(data)
-         })
-       })  
+    
+      const resp = await fetch('https://api.giphy.com/v1/gifs/search?api_key=E1Xs92016grdQgcmMAnfn0bSA6iBk9HV&q=simpsons&limit=10');
+      const data = await resp.json();
+      console.log(data); 
       //console.log(this._historial);//solo para ver que se este grabando
     }
 }
