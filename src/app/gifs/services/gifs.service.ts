86e6cdd76b9e4e34170afffb50abebbf4d7c47c4
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2021-04-19 00:11:25 -0300

    Seccion 7. 85 peticiones http con tipado

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,16 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
-    
-    private apiKey: string = 'E1Xs92016grdQgcmMAnfn0bSA6iBk9HV';
-    private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
 
-    //TODO: cambiar any por su tipo
-    public resultados: any[] = [];
+    public resultados: Gif[] = [];
 
     private _historial: string[] = [];
 
@@ -30,8 +27,8 @@ export class GifsService {
         this._historial = this._historial.splice(0,10);//asigna a historial elemento 0 a 10
       }
 
-      this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=E1Xs92016grdQgcmMAnfn0bSA6iBk9HV&q=${ query }&limit=10`)
-        .subscribe((resp: any) => {
+      this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=E1Xs92016grdQgcmMAnfn0bSA6iBk9HV&q=${ query }&limit=10`)
+        .subscribe((resp) => {
           console.log(resp.data);
           this.resultados = resp.data;
         });
