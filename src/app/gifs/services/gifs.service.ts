61628b3b7c6cc5008515417d67c1c6a5d7b118f3
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2021-04-20 22:59:50 -0300

    http.get().pipe(catchError(this.errorHandler))

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpErrorResponse } from '@angular/common/http';
 import { Gif, SearchGifsResponse } from '../interfaces/gifs.interface';
+import { throwError} from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +16,22 @@ export class GifsService {
     public resultados: Gif[] = [];
 
     private _historial: string[] = [];
+
+    errorHandler(error: HttpErrorResponse) {
+      if (error.error instanceof ErrorEvent) {
+        // A client-side or network error occurred. Handle it accordingly.
+        console.error('An error occurred:', error.error.message);
+      } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong,
+        console.error(
+          `Backend returned code ${error.status}, ` +
+          `body was: ${error.error}`);
+      }
+      // return an observable with a user-facing error message
+      return throwError(
+        'Something bad happened; please try again later.');
+    }
 
     get historial() {
       return [...this._historial];
@@ -47,6 +66,7 @@ export class GifsService {
         .set('q', query);
 
       this.http.get<SearchGifsResponse>(`${ this.servicioUrl }/search`, {params})
+        .pipe(catchError(this.errorHandler))
         .subscribe((resp) => {
           this.resultados = resp.data;
           localStorage.setItem('resultados', JSON.stringify( this.resultados ) );
